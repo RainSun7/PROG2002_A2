31e47ced145e1ab9095d83b37df4e08b1f16b3d7
RainSun7 <liangjihang0627@163.com>
2024-09-24 19:13:19 +0800

    Fixed some errors in the API

--- databaseMysql/controllerAPI/api-controller.js
+++ databaseMysql/controllerAPI/api-controller.js
@@ -3,13 +3,46 @@ var connection = dbcon.getconnection();
 	connection.connect();
 var express = require('express');
 var router = express.Router();
-router.get("/", (req, res)=>{
-	connection.query("select * from FUNDRAISER", (err, records,fields)=> {
+
+router.get("/Home", (req, res)=>{
+	connection.query("SELECT * FROM FUNDRAISER WHERE active = 1", (err, records,fields)=> {
 		 if (err){
-			 console.error("Error while retrieve the data");
+			 console.error("Error getting fundraiser!");
 		 }else{
 			 res.send(records);
 		 }
 	})
 })
+
+router.get("/Search", (req, res) => {
+    connection.query("SELECT * FROM CATEGORY", (err, records,fields) => {
+        if (err) {
+            console.error("Error getting category!", err);
+        } else {
+            res.send(records);
+        }
+    });
+});
+
+router.get("/Search", (req, res) => {
+    connection.query("SELECT * FROM CATEGORY", (err, records,fields) => {
+        if (err) {
+            console.error("Error getting category!", err);
+        } else {
+            res.send(records);
+        }
+    });
+});
+
+router.get("/fundraiser", (req, res) => {
+    const fundraiserId = req.params.id;
+    connection.query("SELECT * FROM FUNDRAISER WHERE id = ?", [fundraiserId], (err, records) => {
+        if (err) {
+            console.error("Error Getting fundraiser info!", err);
+        } else {
+            res.send(records[0]);
+        }
+    });
+});
+
 module.exports = router;
