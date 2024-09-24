b842fe52828168ac300972cf4acdd1a68633ea3e
RainSun7 <liangjihang0627@163.com>
2024-09-24 17:26:12 +0800

    o create RESTful APIs using NodeJS and ExpressJS

--- databaseMysql/crowdfunding_db.js
+++ databaseMysql/crowdfunding_db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/* const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -10,4 +10,21 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
-  });
+  }); */
+
+var dbDetails = require("./db-details");
+
+var mysql = require('mysql2');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+module.exports = {
+	getconnection: ()=>{
+	return mysql.createConnection({
+		host:dbDetails.host,
+		user:dbDetails.user,
+		password:dbDetails.password,
+		database:dbDetails.database	
+	});
+}
+}
